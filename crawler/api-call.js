f71f912780d299c40606b295e8d3b76d53d6a470
Raj Shah <raj.shah@robosoftin.com>
2020-07-02 09:23:06 +0530

    Added support for request timeout

--- crawler/api-call.js
+++ crawler/api-call.js
@@ -1,3 +1,7 @@
 const axios = require("axios");
 
+const axiosRetry = require("axios-retry");
+
+axiosRetry(axios, { retries: 10, retryDelay: 1000 });
+
 module.exports.makeGetRequest = (url) => axios.get(url).then((res) => res.data);
